b7e3463cf3c20580f47222af9c5e7d551eb2163e
Stephen <sdclarkie@gmail.com>
2020-12-16 14:33:05 -0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const xml2json = (srcDOM) => {
+  let children = [...srcDOM.children];
+  // base case for recursion.
+  if (!children.length) {
+    return srcDOM.innerHTML;
+  }
+  // initializing object to be returned.
+  let jsonResult = {};
+  for (let child of children) {
+    // checking is child has siblings of same name.
+    let childIsArray =
+      children.filter((eachChild) => eachChild.nodeName === child.nodeName)
+        .length > 1;
+    // if child is array, save the values as array, else as strings.
+    if (childIsArray) {
+      if (jsonResult[child.nodeName] === undefined) {
+        jsonResult[child.nodeName] = [xml2json(child)];
+      } else {
+        jsonResult[child.nodeName].push(xml2json(child));
+      }
+    } else {
+      jsonResult[child.nodeName] = xml2json(child);
+    }
+  }
+
+  return jsonResult;
+};
 function App() {
   const initialCityState = { city: "", state: "" };
+  const parser = new DOMParser();
+  // eslint-disable-next-line
   const [cityState, setCityState] = useState(initialCityState);
   const [zipcode, setZipcode] = useState("");
+
+  useEffect(() => {
+    const fetchCityState = async () => {
+      try {
+        const response = await fetch(
+          `/.netlify/functions/getCityState?&zipcode=${zipcode}`,
+          {
+            headers: { accept: "application/json" },
+          }
+        );
+        const data = await response.text();
+        const srcDOM = parser.parseFromString(data, "application/xml");
+        const res = xml2json(srcDOM);
+        console.log(res);
+        setCityState({ ...cityState, city: "", state: "" });
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchCityState();
+  }, [zipcode]);
+
   return (
     <div className="App">
       <h1>City/State Lookup Tool</h1>
